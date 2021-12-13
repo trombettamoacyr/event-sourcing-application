115a0a4cf4f605e9fab5918cb0e31e56d5c446cb
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-13 00:48:09 -0300

    aws configuration

--- aws/localstack.sh
+++ aws/localstack.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-## configuration
+## jq install
+apt-get install -y jq
+
+## aws configuration
 aws configure set aws_access_key_id "ID_KEY"
 aws configure set aws_secret_access_key "SECRET_KEY"
 aws configure set region "us-east-1"
@@ -11,11 +14,8 @@ ARN_TOPIC_EVENT_CREATED=$(aws --endpoint-url=http://localhost:4566 sns create-to
 
 ## sqs
 QUEUE_EVENT_CREATED="event_created_queue"
-ARN_QUEUE_EVENT_CREATED=$(aws --endpoint-url=http://localhost:4566 sqs create-queue --name "$QUEUE_EVENT_CREATED" --attribute-names All --output text --query 'Attributes.QueueArn')
+URL_QUEUE_EVENT_CREATED=$(aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name "$QUEUE_EVENT_CREATED" --output text)
+ARN_QUEUE_EVENT_CREATED=$(aws --endpoint-url=http://localhost:4566 sqs get-queue-attributes --queue-url "$URL_QUEUE_EVENT_CREATED" | jq -r ".Attributes.QueueArn")
 
 ## subscription
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "$ARN_TOPIC_EVENT_CREATED" --protocol sqs --notification-endpoint "$ARN_QUEUE_EVENT_CREATED"
-
-## check configuration
-aws --endpoint-url http://localhost:4566 sns list-subscriptions
-curl http://localhost:4566/health
