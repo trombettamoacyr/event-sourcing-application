ea8cf4d0ba60b61c8e7f5df87d5dfa011ffd8115
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-17 02:02:50 -0300

    created AbstractEvent and AbstractListener

--- src/main/java/application/domain/event/AbstractEvent.java
+++ src/main/java/application/domain/event/AbstractEvent.java
@@ -3,13 +3,10 @@ package application.domain.event;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 public abstract class AbstractEvent<T> {
 
-    protected UUID userId;
     protected T payload;
 
 }
--- src/main/java/application/domain/event/AbstractListener.java
+++ src/main/java/application/domain/event/AbstractListener.java
@@ -1,12 +1,20 @@
 package application.domain.event;
 
+import application.domain.entity.EventEntity;
+import application.domain.repository.EventRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 @SuppressWarnings("rawtypes")
 public abstract class AbstractListener<T extends AbstractEvent> {
 
-    private static final String NEW_EVENT = "START EVENT {} - userId {}";
+    private static final String NEW_EVENT = "START EVENT {}";
+    private static final String ERROR_EVENT = "ERROR EVENT {}";
+
+    @Autowired
+    private EventRepository eventRepository;
 
     public abstract void listener(T event);
 
@@ -15,15 +23,42 @@ public abstract class AbstractListener<T extends AbstractEvent> {
     public void resolver(T event) {
 
         var eventName = event.getClass().getSimpleName();
-        var userId = event.getUserId();
-
-        log.info(NEW_EVENT, eventName, userId);
+        log.info(NEW_EVENT, eventName);
 
         try {
             handler(event);
-            log.info("OK");
+            saveEvent(event);
+
         } catch (Exception ex) {
-            log.info("ERRO");
+            saveEventError(event, ex);
+            log.info(ERROR_EVENT, eventName);
         }
+    }
+
+    private void saveEvent(T event) {
+        var name = event.getClass().getSimpleName();
+        var payload = event.getPayload();
+
+        var eventEntity = EventEntity.builder()
+                .name(name)
+                .payload(payload)
+                .build();
+
+        eventRepository.save(eventEntity);
+    }
+
+    private void saveEventError(T event, Exception ex) {
+        var name = event.getClass().getSimpleName();
+        var payload = event.getPayload();
+
+        var eventEntity = EventEntity.builder()
+                .name(name)
+                .payload(payload)
+                .error(true)
+                .exception(ex.getMessage())
+                .stackTrace(ExceptionUtils.getStackTrace(ex))
+                .build();
+
+        eventRepository.save(eventEntity);
     }
 }
