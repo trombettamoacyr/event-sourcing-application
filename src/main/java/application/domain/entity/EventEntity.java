5a4cab2ce976832dcee2aabfd2f900097d5ec07f
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-17 02:01:40 -0300

    add new attributes in EventEntity

--- src/main/java/application/domain/entity/EventEntity.java
+++ src/main/java/application/domain/entity/EventEntity.java
@@ -1,5 +1,6 @@
 package application.domain.entity;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @Document(collection = "event")
 public class EventEntity {
 
@@ -15,6 +17,15 @@ public class EventEntity {
     private String id;
 
     private String name;
+
+    private Object payload;
+
+    @Builder.Default
+    private boolean error = false;
+
+    private String exception;
+
+    private String stackTrace;
 
     @CreatedDate
     private LocalDateTime createdAt;
