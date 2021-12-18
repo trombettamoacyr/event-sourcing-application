133f8820c2b6443c4297a3b7fd3b1bf3bfedf8f4
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-18 19:01:02 -0300

    webclient configuration

---
+++ src/main/java/application/configuration/WebClientConfiguration.java
@@ -0,0 +1,20 @@
+package application.configuration;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @Primary
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
