26ed28c7e51f45cd6043db3e338dffb9f8d35395
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-18 19:03:14 -0300

    new repositories

--- src/main/java/application/domain/repository/CarRepository.java
+++ src/main/java/application/domain/repository/CarRepository.java
@@ -1,9 +1,9 @@
 package application.domain.repository;
 
-import application.domain.entity.EventEntity;
+import application.domain.entity.CarEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends MongoRepository<EventEntity, String> {
+public interface CarRepository extends MongoRepository<CarEntity, String> {
 }
--- src/main/java/application/domain/repository/UserRepository.java
+++ src/main/java/application/domain/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package application.domain.repository;
 
-import application.domain.entity.EventEntity;
+import application.domain.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends MongoRepository<EventEntity, String> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 }
