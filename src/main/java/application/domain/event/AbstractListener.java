381a948d86246f80599d78f1bc1c23ab0ee478da
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-23 03:01:11 -0300

    feature event retry

--- src/main/java/application/domain/event/AbstractListener.java
+++ src/main/java/application/domain/event/AbstractListener.java
@@ -5,6 +5,7 @@ import application.domain.repository.EventRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 
 @Slf4j
 @SuppressWarnings("rawtypes")
@@ -14,8 +15,13 @@ public abstract class AbstractListener<T extends AbstractEvent> {
     private static final String SAVE_EVENT = "SAVE EVENT {}";
     private static final String ERROR_EVENT = "ERROR EVENT {}";
 
+    private static final String EVENT_PACKAGE = "application.domain.event";
+
     @Autowired
     private EventRepository eventRepository;
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
 
     public abstract void listener(T event);
 
@@ -62,5 +68,19 @@ public abstract class AbstractListener<T extends AbstractEvent> {
                 .build();
 
         eventRepository.save(eventEntity);
+
+        var abstractEvent = getEvent(name, payload);
+        applicationEventPublisher.publishEvent(abstractEvent);
+    }
+
+    private AbstractEvent getEvent(String eventName, Object payload) {
+        try {
+            var clazz = Class.forName(EVENT_PACKAGE.concat(".").concat(eventName));
+            var constructor = clazz.getDeclaredConstructors()[0];
+            return (AbstractEvent) constructor.newInstance(payload);
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 }
